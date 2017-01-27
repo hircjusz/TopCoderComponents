0bb6970d28f2ce1a3d9179d39eb4e974807d55c3
DESKTOP-HPRUNBD\Darek <darek.uchman@gmail.com>
2017-01-28 00:32:04 +0100

    TopCoder ObjectSerialization

---
+++ GenericBasicTypeSerialization/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericBasicTypeSerialization
+{
+    //http://software.topcoder.com/catalog/c_component.jsp?comp=27159164&ver=1
+    public class Class1
+    {
+    }
+}
